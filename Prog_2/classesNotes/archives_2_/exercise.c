73fc7f068b8cda7096141ead7fb467fac6dc0f46
BrunoYHayashi <byh25@inf.ufpr.br>
2026-04-03 17:53:15 -0300

    [ADD] anotações arquivos + enunciado exercício

---
+++ Prog_2/classesNotes/archives_2_/exercise.c
@@ -0,0 +1,12 @@
+/*
+Considere o arquivo fornecido junto com o material desta aula para a
+realização de testes. Este arquivo contém um texto em inglês.
+Desenvolva um programa que receba o nome, abra um arquivo texto e
+faça uma varredura em todas as suas palavras, satisfazendo o seguinte
+requisito para apresentar o resultado:
+● Identifique e conte as ocorrências da palavra mais frequente
+    ○ Em empates, retorne qualquer uma das palavras mais
+    frequentes
+    ○ Nenhuma palavra terá mais do que 100 caracteres
+    ○ Não se preocupe com pontuações
+*/
